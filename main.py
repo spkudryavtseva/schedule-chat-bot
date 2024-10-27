7cd8c8b6a34c54b428b21a5f984c32aaa0e29d09
spkudryavtseva <spkudryavtseva@joom.com>
2024-10-27 14:15:53 +0300

    telegram bot start

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-from fastapi import FastAPI
-import uvicorn
+# from fastapi import FastAPI
+# import uvicorn
+#
+#
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# def root():
+#     return {"msg": "Hello World!"}
+#
+#
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+import config
+from handlers import router
 
 
-app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return {"msg": "Hello World!"}
+async def main():
+    bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
--- test_api.py
+++ test_api.py
@@ -6,7 +6,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World!"}
+# def test_read_main():
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"msg": "Hello World!"}
